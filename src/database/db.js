9c31e8ba3799344519ed12c862a4102865214a60
Karla-Silva <mkarla@outlook.com>
2022-11-20 23:33:39 -0300

    testando postgres

--- src/database/db.js
+++ src/database/db.js
@@ -4,18 +4,14 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const user = process.env.POSTGRES_USERNAME;
+/* const user = process.env.POSTGRES_USERNAME;
 const password = process.env.POSTGRES_PASSWORD;
 const host = process.env.POSTGRES_HOST;
 const port = process.env.POSTGRES_PORT;
-const database = process.env.POSTGRES_DATABASE;
+const database = process.env.POSTGRES_DATABASE; */
 
 const db = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database
+  connectionString:process.env.DATABASE_URL
 });
 
 /* const query = db.query('SELECT * FROM crmuser;');
